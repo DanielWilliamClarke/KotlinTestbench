675e69c687d132953c16d07f2e680aa98713cc63
dan.clarke <clarkit@gmail.com>
2025-06-09 15:23:25 +0100

    Add transform method to make things more reactive
    add proper error response for timeout endpoint

--- rest-kotlin-quickstart/src/main/kotlin/org/acme/GreetingResource.kt
+++ rest-kotlin-quickstart/src/main/kotlin/org/acme/GreetingResource.kt
@@ -1,9 +1,11 @@
 package org.acme
 
 import io.smallrye.mutiny.Uni
+import io.smallrye.mutiny.infrastructure.Infrastructure
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.ws.rs.*
 import jakarta.ws.rs.core.MediaType
+import jakarta.ws.rs.core.Response
 import org.acme.rest.Greeting
 import java.time.Duration
 
@@ -22,27 +24,44 @@ class ReactiveGreetingResource(
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/hello/{name}")
     fun hello(@PathParam("name") name: String): Uni<Greeting> {
-        return Uni.createFrom().item(service.greeting(name))
+        return Uni.createFrom().item(name)
+            .emitOn(Infrastructure.getDefaultWorkerPool())
+            .onItem()
+            .transform { n -> service.greeting(n) }
     }
 
     @GET
     @Path("/delayed/{name}")
     @Produces(MediaType.APPLICATION_JSON)
     fun delayed(@PathParam("name") name: String): Uni<Greeting> {
-        return Uni.createFrom().item(service.greeting(name))
-            .onItem().delayIt().by(Duration.ofSeconds(1))
+        return Uni.createFrom().item(name)
+            .emitOn(Infrastructure.getDefaultWorkerPool()) // shift blocking work off IO thread
+            .onItem()
+            .transform { n -> service.greeting(n) } // sync transform
+            .onItem()
+            .delayIt()
+            .by(Duration.ofSeconds(1)) // simulate delay
     }
 
     @GET
     @Path("/timeout/{name}")
     @Produces(MediaType.APPLICATION_JSON)
     fun delayedTimeout(@PathParam("name") name: String): Uni<Greeting> {
-        return Uni.createFrom().item { service.greeting(name) }
-            .onItem().delayIt().by(Duration.ofSeconds(2))
+        return Uni.createFrom().item(name)
+            .emitOn(Infrastructure.getDefaultWorkerPool()) // shift blocking work off IO thread
+            .onItem()
+            .transform { n -> service.greeting(n) } // sync transform
+            .onItem()
+            .delayIt().by(Duration.ofSeconds(2))
             .ifNoItem().after(Duration.ofSeconds(1))
             .fail()
-            .onFailure().recoverWithUni(
-                Uni.createFrom().failure(BadRequestException("Request timed out before completing"))
-            )
+            .onFailure()
+            .recoverWithUni { _ ->
+                val errorResponse = Response
+                    .status(Response.Status.BAD_REQUEST)
+                    .entity(mapOf("error" to "Request timed out before completing"))
+                    .build()
+                Uni.createFrom().failure(WebApplicationException(errorResponse))
+            }
     }
 }
